f6c444d4bb97099e84d2c80bbcc38180250d2444
Raymond Yang <rhyang@stanford.edu>
2022-05-08 18:30:03 -0700

    layout setup

---
+++ design/analog_modulator/layout/get_std_cells.sh
@@ -0,0 +1,11 @@
+# Get layout of standard cells used in design
+
+cp $PDK_ROOT/sky130A/libs.ref/sky130_fd_sc_hd/mag/sky130_fd_sc_hd__buf_2.mag ./
+cp $PDK_ROOT/sky130A/libs.ref/sky130_fd_sc_hd/mag/sky130_fd_sc_hd__clkdlybuf4s50_1.mag ./ 
+cp $PDK_ROOT/sky130A/libs.ref/sky130_fd_sc_hd/mag/sky130_fd_sc_hd__clkinv_1.mag ./ 
+cp $PDK_ROOT/sky130A/libs.ref/sky130_fd_sc_hd/mag/sky130_fd_sc_hd__clkinv_4.mag ./ 
+cp $PDK_ROOT/sky130A/libs.ref/sky130_fd_sc_hd/mag/sky130_fd_sc_hd__dfxbp_1.mag ./ 
+cp $PDK_ROOT/sky130A/libs.ref/sky130_fd_sc_hd/mag/sky130_fd_sc_hd__inv_1.mag ./ 
+cp $PDK_ROOT/sky130A/libs.ref/sky130_fd_sc_hd/mag/sky130_fd_sc_hd__mux2_1.mag ./ 
+cp $PDK_ROOT/sky130A/libs.ref/sky130_fd_sc_hd/mag/sky130_fd_sc_hd__nand2_1.mag ./ 
+cp $PDK_ROOT/sky130A/libs.ref/sky130_fd_sc_hd/mag/sky130_fd_sc_hd__nand2_4.mag ./
--- xyce.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-export PATH=/software/user/singularity-ce/current/bin:$PATH
-export -n MODULEPATH
-unset MODULEPATH
-singularity exec --bind /scratch:/scratch --bind /cad:/cad --bind /farmshare:/farmshare --bind /home:/farmshare/home /home/classes/ee/admin/software/containers/xyce.sif /bin/tcsh
-
-# /farmshare/home/classes/ee/admin/software/bin/xyce.sh
