1157de0314df13ebb0ec3e5615635d809d43b4c6
Raymond Yang <rhyang@stanford.edu>
2022-05-15 18:49:56 -0700

    change signal ports to li layer

--- design/analog_modulator/layout/get_std_cells.sh
+++ design/analog_modulator/layout/get_std_cells.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 # Get layout of standard cells used in design
 STD_LAYOUT_PATH=$PDK_ROOT/sky130A/libs.ref/sky130_fd_sc_hd/mag
 LOCAL_PATH=stdcells
@@ -6,9 +8,11 @@ cp $STD_LAYOUT_PATH/sky130_fd_sc_hd__buf_2.mag ./$LOCAL_PATH
 cp $STD_LAYOUT_PATH/sky130_fd_sc_hd__clkdlybuf4s50_1.mag ./$LOCAL_PATH 
 cp $STD_LAYOUT_PATH/sky130_fd_sc_hd__clkinv_1.mag ./$LOCAL_PATH 
 cp $STD_LAYOUT_PATH/sky130_fd_sc_hd__clkinv_4.mag ./$LOCAL_PATH 
+cp $STD_LAYOUT_PATH/sky130_fd_sc_hd__clkinv_16.mag ./$LOCAL_PATH
 cp $STD_LAYOUT_PATH/sky130_fd_sc_hd__clkbuf_4.mag ./$LOCAL_PATH 
 cp $STD_LAYOUT_PATH/sky130_fd_sc_hd__dfxbp_1.mag ./$LOCAL_PATH 
 cp $STD_LAYOUT_PATH/sky130_fd_sc_hd__inv_1.mag ./$LOCAL_PATH 
 cp $STD_LAYOUT_PATH/sky130_fd_sc_hd__mux2_1.mag ./$LOCAL_PATH 
 cp $STD_LAYOUT_PATH/sky130_fd_sc_hd__nand2_1.mag ./$LOCAL_PATH 
 cp $STD_LAYOUT_PATH/sky130_fd_sc_hd__nand2_4.mag ./$LOCAL_PATH
+
