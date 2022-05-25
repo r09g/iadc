e76c3b553222659eddfd63ee0a2550b158e48e32
Raymond Yang <rhyang@stanford.edu>
2022-05-25 15:56:54 -0700

    a_mux4_en lvs pass

---
+++ design/analog_modulator/layout/a_mux4_en/magic_script.tcl
@@ -0,0 +1,56 @@
+set MGC_DESIGN_NAME "a_mux4_en"
+set MGC_PORT_LIST {en s1 s0 in0 in1 in2 in3 out}
+
+# flatten design and make ports
+
+load "${MGC_DESIGN_NAME}.mag"
+box 0 0 0 0
+select
+
+flatten "${MGC_DESIGN_NAME}_flat"
+
+load "${MGC_DESIGN_NAME}_flat"
+box 0 0 0 0
+select
+
+foreach port $MGC_PORT_LIST {
+    findlabel $port
+    port make
+}
+
+# special handling of power ports
+for {set i 0} {$i < 16} {incr i} {
+    findlabel VDD $i
+    port make 
+    port use power
+    port class inout
+}
+
+for {set i 0} {$i < 17} {incr i} {
+    findlabel VSS $i
+    port make 
+    port use power
+    port class inout
+}
+
+save "${MGC_DESIGN_NAME}_flat.mag"
+
+# lvs extraction
+extract all
+ext2spice lvs
+ext2spice -o "../../netlist/${MGC_DESIGN_NAME}/${MGC_DESIGN_NAME}_layout_lvs.spice"
+
+# pex extraction
+ext2spice cthresh 0
+ext2spice -o "../../netlist/${MGC_DESIGN_NAME}/${MGC_DESIGN_NAME}_layout_pex.spice"
+
+# output gds and lef
+# gds write "${MGC_DESIGN_NAME}.gds"
+# lef write "${MGC_DESIGN_NAME}.lef"
+
+# unset variables
+unset MGC_DESIGN_NAME
+unset MGC_PORT_LIST
+
+quit
+
---
+++ design/analog_modulator/layout/a_mux4_en/run_post_layout_sim.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# run post-layout sim with OTA
+
+# PLSIM_DESIGN_NAME="a_mux4_en" 
+
+# cat ../../netlist/${PLSIM_DESIGN_NAME}/${PLSIM_DESIGN_NAME}_layout_pex.spice ${DESIGN_DIR}/verification/post_layout_sim/${PLSIM_DESIGN_NAME}_tb_template.spice > ${DESIGN_DIR}/verification/post_layout_sim/${PLSIM_DESIGN_NAME}_tb.spice
+# ngspice ${DESIGN_DIR}/verification/post_layout_sim/${PLSIM_DESIGN_NAME}_tb.spice
+
