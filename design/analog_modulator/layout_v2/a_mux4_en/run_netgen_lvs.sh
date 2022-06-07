e95c8e0eedce5d5710f2a73776f92d26251ab8c7
Raymond Yang <rhyang@stanford.edu>
2022-06-07 04:40:37 -0700

    layout

--- design/analog_modulator/layout_v2/a_mux4_en/run_netgen_lvs.sh
+++ design/analog_modulator/layout_v2/a_mux4_en/run_netgen_lvs.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 LVS_DESIGN_NAME="a_mux4_en"
-netgen -batch lvs "../../netlist/${LVS_DESIGN_NAME}/${LVS_DESIGN_NAME}.spice ${LVS_DESIGN_NAME}" "../../netlist/${LVS_DESIGN_NAME}/${LVS_DESIGN_NAME}_layout_lvs.spice ${LVS_DESIGN_NAME}_flat" ../sky130A_setup.tcl
+netgen -batch lvs "../../netlist/${LVS_DESIGN_NAME}/${LVS_DESIGN_NAME}.spice ${LVS_DESIGN_NAME}" "../../netlist/${LVS_DESIGN_NAME}/${LVS_DESIGN_NAME}_layout_lvs.spice ${LVS_DESIGN_NAME}" ../sky130A_setup.tcl
